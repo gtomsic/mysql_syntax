9abca30acbdf01a62d68aff5b50bdf5c12b3c4d7
Gabriel Tomsic <gabriel@mac.local>
2024-11-24 21:18:35 -0600

    Select All Filter BETWEEN

---
+++ select_all_between.sql
@@ -0,0 +1,7 @@
+-- Obtain all information pertaining all city ID's between 500 and 525
+SELECT * FROM city 
+WHERE ID BETWEEN 500 AND 525;
+
+-- Obtain all information pertaining cities with population between 1000 and 2000
+SELECT * FROM city
+WHERE Population BETWEEN 1000 AND 2000;
