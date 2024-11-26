1c1038d35d4da28dac6440250e50a48451978de8
Gabriel Tomsic <gabriel@mac.local>
2024-11-25 21:12:42 -0600

    Select and used of Mathematical FUNCTION

--- select_MATHEMATICAL_FUNCTION.sql
+++ select_MATHEMATICAL_FUNCTION.sql
@@ -3,11 +3,11 @@ SELECT 	payment_id,
 		amount, 
         amount * -1 AS `negative amount`, -- change the amount value to negative
         ABS(amount * -1), -- change the negative value to positive
-        CEIL(amount), -- ceil is roundup to whole number
-        FLOOR(amount), -- remove the decimal point()
-        ROUND(amount, 1), -- round to whole number and control the decimal
-        TRUNCATE(amount, 1), -- Control the value after the decimal point 
-        POW(payment_id, 3), -- Power of 3 payment_id  
+        CEIL(amount), -- Round up to the nearest whole integer
+        FLOOR(amount), -- Round down to the nearest whole integer
+        ROUND(amount, 1), -- Round to specified the number of decimal places
+        TRUNCATE(amount, 1), -- Remove the values from the end of your value (no rounding)
+        POW(payment_id, 3), -- Raise the value to Power of 3 payment_id  
         POWER(payment_id, 3), -- Same as POW result
         ROUND(SQRT(payment_id), 2), -- Square root of each value
         PI()
