7ab45e1d56166e97ed5940199737634e7debf84e
Gabriel Tomsic <gabriel@mac.local>
2024-11-26 08:23:52 -0600

    Working with Date FUNCTIONS 2nd example

---
+++ DATE_FUNCTIONS_2.sql
@@ -0,0 +1,12 @@
+SELECT 	CURDATE(),
+		CURRENT_DATE(),
+        CURTIME(),
+        CURRENT_TIME(),
+        NOW(),
+        CURRENT_TIMESTAMP(),
+        SYSDATE(),
+        SLEEP(5),
+        NOW(),
+        SYSDATE()
+FROM rental
+LIMIT 1;
