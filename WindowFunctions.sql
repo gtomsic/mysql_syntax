35d6977b848888c844d18c77e38713d33cf54d88
Gabriel Tomsic <gabriel@mac.local>
2024-11-26 07:46:16 -0600

    Using Window Functions

---
+++ WindowFunctions.sql
@@ -0,0 +1,17 @@
+-- Window Functions Examples
+SELECT * FROM country;
+
+SELECT 	Name,
+		Continent,
+        LifeExpectancy,
+        RANK() OVER (ORDER BY LifeExpectancy DESC) AS LE_Rank,
+		DENSE_RANK() OVER (ORDER BY LifeExpectancy DESC) AS LE_Dense_Rank,
+        PERCENT_RANK() OVER(PARTITION BY Continent
+							ORDER BY LifeExpectancy) AS LE_Percent_Rank,
+		ROW_NUMBER() OVER(ORDER BY LifeExpectancy DESC) AS Row_Num,
+        FIRST_VALUE(LifeExpectancy) OVER(PARTITION BY Continent
+										ORDER BY LifeExpectancy DESC) AS first_val,
+		LAST_VALUE(Name) OVER(PARTITION BY Continent
+										ORDER BY LifeExpectancy DESC) AS first_val
+FROM country
+ORDER BY Continent, LifeExpectancy DESC;
